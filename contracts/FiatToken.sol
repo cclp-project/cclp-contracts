18dab24457141d36206d2609d067096282e336d8
UnclueVUltur <cryptowea@gmail.com>
2019-02-27 17:53:21 -0300

    masterminter contract

--- contracts/FiatToken.sol
+++ contracts/FiatToken.sol
@@ -4,14 +4,17 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
+import "./MasterMinter.sol";
 
-contract FiatToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable {
+contract FiatToken is ERC20, ERC20Detailed, ERC20Mintable, ERC20Burnable, MasterMinter {
 
     constructor(
         string memory name,
         string memory symbol,
-        uint8 decimals
+        uint8 decimals,
+        address masterMinterAddress
     )
+        MasterMinter(masterMinterAddress)
         ERC20Burnable()
         ERC20Mintable()
         ERC20Detailed(name, symbol, decimals)
--- migrations/2_deploy_cclp.js
+++ migrations/2_deploy_cclp.js
@@ -5,7 +5,8 @@ module.exports = (deployer, network, accounts) => {
     const name="cCLP Fiat Token"
     const symbol="cCLP"
     const decimals = 18
+    const masterMinterAddress = '0xad27fcf246d1a9752daf649d6391c17c1ce80d0c';
     
-    deployer.deploy(FiatToken,name,symbol,decimals);
+    deployer.deploy(FiatToken,name,symbol,decimals, masterMinterAddress);
 }
 
