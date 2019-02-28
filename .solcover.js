b39f3a0f14218c2af3310578e619651aaf4e7468
Andres Junge <ajunge@yaykuy.cl>
2019-02-28 10:35:19 +0100

    add solidity-coverage. Resolves #2

---
+++ .solcover.js
@@ -0,0 +1,5 @@
+module.exports = {
+    copyPackages: ['openzeppelin-solidity'],
+    compileCommand: '../node_modules/.bin/truffle compile',
+    testCommand: '../node_modules/.bin/truffle test --network coverage',
+};
--- truffle-config.js
+++ truffle-config.js
@@ -24,7 +24,13 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    },
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
