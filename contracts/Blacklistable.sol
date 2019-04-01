07642a8243cf008db2fd5cf96ae0a51d611f09b1
Andres Junge <ajunge@yaykuy.cl>
2019-04-01 12:28:42 -0300

    blacklister

---
+++ contracts/Blacklistable.sol
@@ -0,0 +1,70 @@
+pragma solidity ^0.5.0;
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * @title Blacklistable Token
+ * @dev Allows accounts to be blacklisted by a "blacklister" role
+*/
+contract Blacklistable is Ownable {
+
+    address public blacklister;
+    mapping(address => bool) internal blacklisted;
+
+    event Blacklisted(address indexed _account);
+    event UnBlacklisted(address indexed _account);
+    event BlacklisterChanged(address indexed newBlacklister);
+
+    constructor(address _blacklister) public {
+        blacklister = _blacklister;
+        emit BlacklisterChanged(blacklister);
+    }
+
+    /**
+     * @dev Throws if called by any account other than the blacklister
+    */
+    modifier onlyBlacklister() {
+        require(msg.sender == blacklister);
+        _;
+    }
+
+    /**
+     * @dev Throws if argument account is blacklisted
+     * @param _account The address to check
+    */
+    modifier notBlacklisted(address _account) {
+        require(blacklisted[_account] == false);
+        _;
+    }
+
+    /**
+     * @dev Checks if account is blacklisted
+     * @param _account The address to check    
+    */
+    function isBlacklisted(address _account) public view returns (bool) {
+        return blacklisted[_account];
+    }
+
+    /**
+     * @dev Adds account to blacklist
+     * @param _account The address to blacklist
+    */
+    function blacklist(address _account) public onlyBlacklister {
+        blacklisted[_account] = true;
+        emit Blacklisted(_account);
+    }
+
+    /**
+     * @dev Removes account from blacklist
+     * @param _account The address to remove from the blacklist
+    */
+    function unBlacklist(address _account) public onlyBlacklister {
+        blacklisted[_account] = false;
+        emit UnBlacklisted(_account);
+    }
+
+    function updateBlacklister(address _newBlacklister) public onlyOwner {
+        require(_newBlacklister != address(0), "blacklister is 0x0");
+        blacklister = _newBlacklister;
+        emit BlacklisterChanged(blacklister);
+    }
+}
